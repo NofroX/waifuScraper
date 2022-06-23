bf43701e815dd2f754dedf3f8956d3d0a5a15868
NofroX <74203655+NofroX@users.noreply.github.com>
2022-06-23 11:45:34 +0200

    .

--- Parser.py
+++ Parser.py
@@ -33,7 +33,7 @@ class WaifuHTMLParser(HTMLParser):
             add_character(self.name, data)
             self.series_state = False
 
-
+def scrape_page():
 site = "https://mywaifulist.moe/husbandos"
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
